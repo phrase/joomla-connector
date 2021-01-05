90c980941735a9d0f06f8535cb61b8b813c655c6
Ludek Vesely <ludek.vesely@memsource.com>
2021-01-05 15:46:30 +0100

    v1.0.1 - Add missing file

---
+++ site/api/account.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (C) 2019 Memsource a.s. All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+/**
+ * @since 1.0.0
+ */
+class MemsourceConnectorApiAccount extends MemsourceConnectorApiBase
+{
+	/**
+	 * Get basic server info.
+	 *
+	 * @return  array
+	 *
+	 * @since   1.0.0
+	 */
+	public function getList()
+	{
+		return array(
+			'account' => array(
+				'baseUri' => JUri::base(),
+				'currentUri' => JUri::current()
+			)
+		);
+	}
+}
